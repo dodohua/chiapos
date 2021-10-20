9f2c549016a7a55947007495819ef3d01c21f834
allenlinkme <allenlinkme@gmail.com>
2021-10-20 14:04:03 +0800

    remove lock

--- src/prover_disk.hpp
+++ src/prover_disk.hpp
@@ -142,55 +142,51 @@ public:
     {
         std::vector<LargeBits> qualities;
 
-        std::lock_guard<std::mutex> l(_mtx);
+        std::ifstream disk_file(filename, std::ios::in | std::ios::binary);
 
-        {
-            std::ifstream disk_file(filename, std::ios::in | std::ios::binary);
+        if (!disk_file.is_open()) {
+            throw std::invalid_argument("Invalid file " + filename);
+        }
 
-            if (!disk_file.is_open()) {
-                throw std::invalid_argument("Invalid file " + filename);
-            }
+        // This tells us how many f7 outputs (and therefore proofs) we have for this
+        // challenge. The expected value is one proof.
+        std::vector<uint64_t> p7_entries = GetP7Entries(disk_file, challenge);
 
-            // This tells us how many f7 outputs (and therefore proofs) we have for this
-            // challenge. The expected value is one proof.
-            std::vector<uint64_t> p7_entries = GetP7Entries(disk_file, challenge);
+        if (p7_entries.empty()) {
+            return std::vector<LargeBits>();
+        }
 
-            if (p7_entries.empty()) {
-                return std::vector<LargeBits>();
-            }
+        // The last 5 bits of the challenge determine which route we take to get to
+        // our two x values in the leaves.
+        uint8_t last_5_bits = challenge[31] & 0x1f;
 
-            // The last 5 bits of the challenge determine which route we take to get to
-            // our two x values in the leaves.
-            uint8_t last_5_bits = challenge[31] & 0x1f;
+        for (uint64_t position : p7_entries) {
+            // This inner loop goes from table 6 to table 1, getting the two backpointers,
+            // and following one of them.
+            for (uint8_t table_index = 6; table_index > 1; table_index--) {
+                uint128_t line_point = ReadLinePoint(disk_file, table_index, position);
 
-            for (uint64_t position : p7_entries) {
-                // This inner loop goes from table 6 to table 1, getting the two backpointers,
-                // and following one of them.
-                for (uint8_t table_index = 6; table_index > 1; table_index--) {
-                    uint128_t line_point = ReadLinePoint(disk_file, table_index, position);
+                auto xy = Encoding::LinePointToSquare(line_point);
+                assert(xy.first >= xy.second);
 
-                    auto xy = Encoding::LinePointToSquare(line_point);
-                    assert(xy.first >= xy.second);
-
-                    if (((last_5_bits >> (table_index - 2)) & 1) == 0) {
-                        position = xy.second;
-                    } else {
-                        position = xy.first;
-                    }
+                if (((last_5_bits >> (table_index - 2)) & 1) == 0) {
+                    position = xy.second;
+                } else {
+                    position = xy.first;
                 }
-                uint128_t new_line_point = ReadLinePoint(disk_file, 1, position);
-                auto x1x2 = Encoding::LinePointToSquare(new_line_point);
-
-                // The final two x values (which are stored in the same location) are hashed
-                std::vector<unsigned char> hash_input(32 + Util::ByteAlign(2 * k) / 8, 0);
-                memcpy(hash_input.data(), challenge, 32);
-                (LargeBits(x1x2.second, k) + LargeBits(x1x2.first, k))
-                    .ToBytes(hash_input.data() + 32);
-                std::vector<unsigned char> hash(picosha2::k_digest_size);
-                picosha2::hash256(hash_input.begin(), hash_input.end(), hash.begin(), hash.end());
-                qualities.emplace_back(hash.data(), 32, 256);
             }
-        }  // Scope for disk_file
+            uint128_t new_line_point = ReadLinePoint(disk_file, 1, position);
+            auto x1x2 = Encoding::LinePointToSquare(new_line_point);
+
+            // The final two x values (which are stored in the same location) are hashed
+            std::vector<unsigned char> hash_input(32 + Util::ByteAlign(2 * k) / 8, 0);
+            memcpy(hash_input.data(), challenge, 32);
+            (LargeBits(x1x2.second, k) + LargeBits(x1x2.first, k))
+                    .ToBytes(hash_input.data() + 32);
+            std::vector<unsigned char> hash(picosha2::k_digest_size);
+            picosha2::hash256(hash_input.begin(), hash_input.end(), hash.begin(), hash.end());
+            qualities.emplace_back(hash.data(), 32, 256);
+        }
         return qualities;
     }
 
@@ -442,39 +438,40 @@ public:
     {
         LargeBits full_proof;
 
-        std::lock_guard<std::mutex> l(_mtx);
-        {
-            std::ifstream disk_file(filename, std::ios::in | std::ios::binary);
+//        std::lock_guard<std::mutex> l(_mtx);
+//        {
+//
+//        }  // Scope for disk_file
+        std::ifstream disk_file(filename, std::ios::in | std::ios::binary);
 
-            if (!disk_file.is_open()) {
-                throw std::invalid_argument("Invalid file " + filename);
-            }
+        if (!disk_file.is_open()) {
+            throw std::invalid_argument("Invalid file " + filename);
+        }
 
-            std::vector<uint64_t> p7_entries = GetP7Entries(disk_file, challenge);
-            if (p7_entries.empty() || index >= p7_entries.size()) {
-                throw std::logic_error("No proof of space for this challenge");
-            }
+        std::vector<uint64_t> p7_entries = GetP7Entries(disk_file, challenge);
+        if (p7_entries.empty() || index >= p7_entries.size()) {
+            throw std::logic_error("No proof of space for this challenge");
+        }
 
-            // Gets the 64 leaf x values, concatenated together into a k*64 bit string.
-            std::vector<Bits> xs;
-            if (parallel_read) {
-                xs = GetInputs(p7_entries[index], 6);
-            } else {
-                xs = GetInputs(
+        // Gets the 64 leaf x values, concatenated together into a k*64 bit string.
+        std::vector<Bits> xs;
+        if (parallel_read) {
+            xs = GetInputs(p7_entries[index], 6);
+        } else {
+            xs = GetInputs(
                     p7_entries[index],
                     6,
                     &disk_file);  // Passing in a disk_file disabled the parallel reads
-            }
+        }
 
-            // Sorts them according to proof ordering, where
-            // f1(x0) m= f1(x1), f2(x0, x1) m= f2(x2, x3), etc. On disk, they are not stored in
-            // proof ordering, they're stored in plot ordering, due to the sorting in the Compress
-            // phase.
-            std::vector<LargeBits> xs_sorted = ReorderProof(xs);
-            for (const auto& x : xs_sorted) {
-                full_proof += x;
-            }
-        }  // Scope for disk_file
+        // Sorts them according to proof ordering, where
+        // f1(x0) m= f1(x1), f2(x0, x1) m= f2(x2, x3), etc. On disk, they are not stored in
+        // proof ordering, they're stored in plot ordering, due to the sorting in the Compress
+        // phase.
+        std::vector<LargeBits> xs_sorted = ReorderProof(xs);
+        for (const auto& x : xs_sorted) {
+            full_proof += x;
+        }
         return full_proof;
     }
 
